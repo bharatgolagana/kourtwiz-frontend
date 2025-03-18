dca1794ef8c064de5c39e3377b49b35e0f7f78eb
Keshav-zeta <keshav.g@zetaopt.com>
2025-03-18 15:26:19 +0530

    header

--- src/features/landing/components/header/Header.tsx
+++ src/features/landing/components/header/Header.tsx
@@ -1,56 +1,26 @@
-import "./Header.css";
-import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import logo from "@/assets/logo.png";
+import "./Header.css";
 
 const Header = () => {
-  const [navigation, setNavigation] = useState(false);
   const navigate = useNavigate();
-  const navRef = useRef(null);
-
-  useEffect(() => {
-    const navToggle = () => {
-      if (window.innerWidth < 900) {
-        setNavigation(false);
-      }
-    };
-    window.addEventListener("resize", navToggle);
-    return () => {
-      window.removeEventListener("resize", navToggle);
-    };
-  }, []);
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (navRef.current && !navRef.current.contains(event.target)) {
-        setNavigation(false);
-      }
-    };
-
-    if (navigation) {
-      document.addEventListener("mousedown", handleClickOutside);
-    } else {
-      document.removeEventListener("mousedown", handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [navigation]);
 
   return (
-    <div className="header">
-      <nav className="nav" ref={navRef}>
-        <div className="nav-links">
-          <p><a href="#features" className="nav-spacing">Features </a> <a href="#pricing" className="nav-spacing">Pricing </a>
-          <a href="#about" className="nav-spacing">About </a>
-          <a href="#contact" className="nav-spacing">Contact </a></p>
-        </div>
-        <div className="nav-buttons">
-          <button className="btn" onClick={() => navigate("/login")}>Login</button>
-          <button className="btn primary" onClick={() => navigate("/register")}>Try it Out</button>
-        </div>
+    <header className="header">
+      <div className="logo">
+        <img src={logo} alt="Pickleball Logo"  style={{ width: "50px", height: "auto" }}/>
+      </div>
+      <nav className="nav-links">
+        <a href="/">HOME</a>
+        <a href="/about">ABOUT</a>
+        <a href="/services">SERVICES</a>
+        <a href="/events">EVENTS</a>
+        <a href="/products">PRODUCTS</a>
+        <button className="contact-btn" onClick={() => navigate("/contact")}>
+          CONTACT
+        </button>
       </nav>
-    </div>
+    </header>
   );
 };
 

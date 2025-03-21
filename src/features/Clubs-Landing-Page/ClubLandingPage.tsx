f5de24abd3e2b2e4628d831e7b9a7a93e520dbcb
27neethu <neethu20ucse110@mahindrauniversity.edu.in>
2025-03-21 16:57:58 +0530

    backround image

--- src/features/Clubs-Landing-Page/ClubLandingPage.tsx
+++ src/features/Clubs-Landing-Page/ClubLandingPage.tsx
@@ -1,14 +1,14 @@
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import "./ClubLandingPage.css";
 import pickleball1 from "../../assets/pickleball1.jpg";
-import pickleball3 from "../../assets/pickleball3.jpeg";
-import pickleball4 from "../../assets/pickleball4.jpeg";
+import pickleball3 from "../../assets/69c276be-7548-473e-8ec7-24db9c135a35.jpeg";
+import pickleball4 from "../../assets/b357939a-e94c-4619-a970-08eb5c85570c.jpeg";
 
 const clubs = [
-    { id: "67d261f783c50d6c7df9dd50", name: "New Jersey Club", description: "Premium coaching and top-tier facilities.",image:{pickleball1}, bgColor: "#ffcccb" },
-    { id: "67d2620383c50d6c7df9dd51", name: "Houston Club", description: "A great place for community play and tournaments.",image:{pickleball3}, bgColor: "#d1e7dd" },
-    { id: "67d2cc08c1534675fa24ff39", name: "Manchester-pickball", description: "Train like a pro with experienced instructors.",image:{pickleball4}, bgColor: "#cce5ff" }
-  ];
+    { id: "67d261f783c50d6c7df9dd50", name: "New Jersey Club", description: "Premium coaching and top-tier facilities.", image: pickleball1, bgColor: "#ffcccb" },
+    { id: "67d2620383c50d6c7df9dd51", name: "Houston Club", description: "A great place for community play and tournaments.", image: pickleball3, bgColor: "#d1e7dd" },
+    { id: "67d2cc08c1534675fa24ff39", name: "Manchester-pickball", description: "Train like a pro with experienced instructors.", image: pickleball4, bgColor: "#cce5ff" }
+];
 const ClubLandingPage = () => {
     const { id } = useParams();
     const location = useLocation();
@@ -28,11 +28,13 @@ const ClubLandingPage = () => {
               <button className="btn" onClick={() => navigate("")}>Become a Member</button>
               <button className="btn" onClick={() => navigate(`/clubs/${id}/authentication`, { state: { clubName } })}>Authenticate</button>
             </div>
-      <div className="club-landing" style={{ backgroundColor: bgColor }}>
-          <div className="club-container">
+            <div className="club-landing" style={{ backgroundColor: bgColor }}>
+                <div 
+                    className="club-container" 
+                    style={{ backgroundImage: clubImage ? `url(${clubImage})` : "none" }}
+                >
             <h1>Welcome to {clubName}</h1>
             <p>{description}</p>
-            <img src={clubImage} alt={clubName} className="club-image" />
         </div>
       </div>
       </>

27c7c23bef068b441b69293ae780ccadb81de5af
AnkiZeta112 <ankit.sen@zetaopt.com>
2025-03-29 15:04:50 +0530

    courts setup

--- src/features/club-courts/components/CourtsList.tsx
+++ src/features/club-courts/components/CourtsList.tsx
@@ -1,4 +1,13 @@
+import { useUserInfo } from '../../../context/UserInfoContext';
+import { useGetClubCourt } from '../../../shared/apis/courts/useGetClubCourts';
+
 const CourtsList = () => {
+  const { userInfo } = useUserInfo();
+  const { data: clubList, isLoading } = useGetClubCourt(
+    '67e66d174ac81260061a2a8c'
+  );
+  console.log('club list ', clubList);
+  console.log('userInfo : ', userInfo);
   return <>Courts list</>;
 };
 

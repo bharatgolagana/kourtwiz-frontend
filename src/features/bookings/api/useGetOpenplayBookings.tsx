bd52f0b94d180db5a9decc867fb3128e01cc5672
27neethu <neethu20ucse110@mahindrauniversity.edu.in>
2025-04-11 12:17:04 +0530

    membership plan

---
+++ src/features/bookings/api/useGetOpenplayBookings.tsx
@@ -0,0 +1,30 @@
+import { UseQueryResult, useQuery } from '@tanstack/react-query';
+
+export function useGetOpenplayBookings(userId: string): UseQueryResult<unknown, Error> {
+  const getOpenplayBookings = async () => {
+    try {
+      const token = localStorage.getItem('jwtToken');
+      const response = await fetch(`http://44.216.113.234:8080/api/openplay/bookings/${userId}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch previous bookings');
+      }
+      return await response.json();
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  };
+
+  return useQuery<unknown, Error>({
+    queryKey: ['openplayBookings', userId],
+    queryFn: getOpenplayBookings,
+    enabled: !!userId,
+  });
+}
